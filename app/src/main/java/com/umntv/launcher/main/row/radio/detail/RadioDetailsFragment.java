544b55eb15277bb414dcadde169a79832b6c78b4
x-syaifullah-x <x.syaifullah.x@gmail.com>
2023-08-04 02:49:32 +0700

    update trap beats

--- app/src/main/java/com/umntv/launcher/main/row/radio/detail/RadioDetailsFragment.java
+++ app/src/main/java/com/umntv/launcher/main/row/radio/detail/RadioDetailsFragment.java
@@ -294,9 +294,17 @@ public class RadioDetailsFragment extends DetailsSupportFragment {
             switch (mSelectedRadio.getTitle()) {
                 case Radio.TRAP_BEATS_NAME: {
                     UmnPlayList umnPlayList = TRAP_BEATS_PLAY_LIST[index];
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(umnPlayList.link));
-                    startActivity(i);
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    try {
+                        intent.setData(Uri.parse(umnPlayList.link));
+                        intent.setPackage("com.tcl.browser");
+                        startActivity(intent);
+                    } catch (Throwable t) {
+                        Toast.makeText(getContext(), t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        String uriString = umnPlayList.link;
+                        intent.setData(Uri.parse(uriString));
+                        startActivity(intent);
+                    }
                     break;
                 }
                 case Radio.HMONG_SONGS: {
