eea94c678340d0e227814ec93112d0a80e49f77a
x-syaifullah-x <x.syaifullah.x@gmail.com>
2023-12-29 00:54:30 +0700

    fixed Flurry SDK

--- app/src/main/java/com/umntv/launcher/App.java
+++ app/src/main/java/com/umntv/launcher/App.java
@@ -12,16 +12,16 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-//        new FlurryAgent.Builder()
- //               .withLogEnabled(true)
-  //              .withLogLevel(Log.VERBOSE)
-   //             .withReportLocation(true)
-    //            .withCaptureUncaughtExceptions(true)
-     //           .withContinueSessionMillis(10000)
-      //          .withSessionForceStart(true)
-       //         .withPerformanceMetrics(FlurryPerformance.ALL)
-        //        .withIncludeBackgroundSessionsInMetrics(true)
-         //       .build(this, "68Y5VWW6XTGYSXHFTFYZ");
+        new FlurryAgent.Builder()
+                .withLogEnabled(true)
+                .withLogLevel(Log.VERBOSE)
+                .withReportLocation(true)
+                .withCaptureUncaughtExceptions(true)
+                .withContinueSessionMillis(10000)
+                .withSessionForceStart(true)
+                .withPerformanceMetrics(FlurryPerformance.ALL)
+                .withIncludeBackgroundSessionsInMetrics(true)
+                .build(this, "68Y5VWW6XTGYSXHFTFYZ");
 
 //        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
 //            Executors.newSingleThreadExecutor().execute(new Runnable() {
