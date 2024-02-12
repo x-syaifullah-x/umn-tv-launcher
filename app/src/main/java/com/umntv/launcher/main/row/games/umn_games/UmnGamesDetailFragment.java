4cb1584e45e1c67bb99d4a68bcf9ecf0562c3f7e
x-syaifullah-x <x.syaifullah.x@gmail.com>
2024-02-13 02:03:55 +0700

    fixed umn games

--- app/src/main/java/com/umntv/launcher/main/row/games/umn_games/UmnGamesDetailFragment.java
+++ app/src/main/java/com/umntv/launcher/main/row/games/umn_games/UmnGamesDetailFragment.java
@@ -1,20 +1,13 @@
 package com.umntv.launcher.main.row.games.umn_games;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.umntv.launcher.main.base.ApkData;
 import com.umntv.launcher.main.base.BaseDetailFragment;
 import com.umntv.launcher.main.base.OverviewItem;
-import com.umntv.launcher.main.row.games.umn_games.DataSource;
 import com.umntv.launcher.util.Admob;
-import com.umntv.launcher.util.AndroidStore;
-import com.umntv.launcher.util.view.dialog.ApkUtil;
 
 import media.umn.tv.R;
 
@@ -33,29 +26,23 @@ public class UmnGamesDetailFragment extends BaseDetailFragment {
     @Override
     protected void onActionClickListener(OverviewItem overviewItem) {
         ApkData apkData = overviewItem.apkData;
-        Intent launchIntent = requireContext()
-                .getPackageManager()
-                .getLaunchIntentForPackage(apkData.packageName);
+        Uri a = Uri.parse(apkData.url);
+        Uri u = new Uri.Builder()
+                .scheme("n0render")
+                .authority(a.getAuthority())
+                .path(a.getPath())
+                .build();
+        Intent launchIntent = new Intent(Intent.ACTION_VIEW, u);
+        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         try {
-            if (launchIntent == null) {
-                launchIntent = new Intent();
-                launchIntent.setPackage(apkData.packageName);
-                launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            }
-            launchIntent.setData(Uri.parse(apkData.url));
-//            launchIntent.addCategory("android.intent.category.LEANBACK_LAUNCHER");
             requireContext().startActivity(launchIntent);
         } catch (Throwable e) {
-            ApkData a = new ApkData(
+            ApkData aaa = new ApkData(
                     com.umntv.launcher.main.row.umn_tv.detail.download_center.DataSource.URL_GAME_BROWSER,
                     apkData.packageName,
                     false
             );
-            openOrDownload(a);
-//            Intent i = new Intent(Intent.ACTION_VIEW);
-//            i.setPackage("com.jio.web.androidtv");
-//            i.setData(Uri.parse(apkData.url));
-//            startActivity(i);
+            openOrDownload(aaa);
         }
     }
 }
